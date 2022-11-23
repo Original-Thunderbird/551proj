b820d98b803b1120b862937441b3d75f89e501e4
Zao0824 <925889398@qq.com>
2022-11-23 04:06:36 -0800

    Add query and analysis with MySQL. Support "select from where(group by)" query.

--- server/index.js
+++ server/index.js
@@ -41,56 +41,19 @@ app.post('/create', (req, res) => {
     }
 });
 
-app.post("/upload", (req, res) => {
-    const file = req.files.file;
-  
-    file.mv(path.join(__dirname, '/file/', file.name), (err) => {
-      if (err) {
-        console.log(err)
-        res.status(500).send({ message: "File upload failed", code: 200 });
-      } else{
-        console.log('here')
-        res.send("File Uploaded");
-      }
-    });
+app.post('/put', (req, res) => {
+    console.log(req.body.file);
+    console.log(req.body.file['bookid']);
+    console.log(req.body.numPart);
+    res.send('success');
 });
 
-app.get('/employees', (req, res) => {
-    config.sqlDB.query("SELECT * FROM employees", (err, result) => {
-        if(err) {
-
-            console.log(err)
-        } else {
-
-            res.send(result)
-        }
-    });
-});
 
 app.post('/cmd',  (req, res) => {
     var cmd = req.body.cmd, params = req.body.params, filename = req.body.filename;
     //console.log(cmd, params, filename)
     var content = "1", err = "";
-    route.Route(cmd, params, filename, function (result){res.send({content: result[0].name, err: err});});
-    //res.send({content: "", err: err});
-
-    // switch(handle){
-    //     case 1:
-    //         config.sqlDB.query(
-    //             'INSERT INTO employees (name, age, country) VALUES (?, ?, ?)', 
-    //             [name, age, country], (err, result) => {
-    //                 if(err) {
-    //                     console.log(err)
-    //                 } else {
-    //                     res.send("Vals inserted")
-    //                 }
-    //             }
-    //         );
-    //         break;
-    //     // case 2:
-    //     //     emp.add(req.body)
-    //     //     res.send("Vals inserted")
-    // }
+    route.Route(cmd, params, filename, function (result){res.send({content: result, err: err});});
 });
 
 app.post('/query', (req, res) => {
