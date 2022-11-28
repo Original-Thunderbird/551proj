1412e1d17f522e784764ab778f4e1366e202baca
Original-Thunderbird <haohao_67890@126.com>
2022-11-28 08:20:37 -0800

    fix ls after put

--- client/src/explorer.js
+++ client/src/explorer.js
@@ -176,7 +176,7 @@ export default function Explorer(props) {
   const handleFileRead = (event) => {
     event.preventDefault(event);
     Axios.post('http://localhost:3001/put', {file: JSON.parse(fileReader.result), name: fileName, numPart: numPart}).then((res) => {});
-    Axios.post('http://localhost:3001/cmd', {cmd: 'ls'}).then((res) => {
+    Axios.post('http://localhost:3001/cmd', {cmd: 'ls', params: [explorerState.curDir]}).then((res) => {
       setExplorerState(prevExplorerState => {
         return {
           ...prevExplorerState,
